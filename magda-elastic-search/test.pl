5607ad3dedc5e663a8ebe0614debead2481a6363
Jacky Jiang <jacky.jiang@data61.csiro.au>
2018-06-05 09:44:13 +1000

    added test file

---
+++ magda-elastic-search/test.pl
@@ -0,0 +1,175 @@
+s(100550341,1,'business',n,9,0).
+s(100550341,2,'stage business',n,1,0).
+s(100581885,2,'unfinished business',n,1,0).
+s(100582388,2,'business',n,3,13).
+s(100606227,1,'business life',n,1,0).
+s(100736894,1,'monkey business',n,1,0).
+s(101094725,2,'business enterprise',n,1,1).
+s(101094725,3,'business',n,2,20).
+s(101095753,1,'business activity',n,1,1).
+s(101096245,1,'business',n,6,7).
+s(101096997,1,'land-office business',n,1,0).
+s(101097119,3,'line of business',n,2,0).
+s(101098541,2,'employee-owned business',n,1,0).
+s(101101114,1,'discount business',n,1,1).
+s(101101227,1,'real-estate business',n,1,0).
+s(101103964,3,'meat-packing business',n,1,0).
+s(101104406,1,'agribusiness',n,1,0).
+s(101110274,3,'business deal',n,1,0).
+s(101119401,1,'going-out-of-business sale',n,1,0).
+s(102690081,2,'airline business',n,1,0).
+s(102925519,1,'business suit',n,1,0).
+s(103671668,5,'business line',n,1,0).
+s(103671668,6,'line of business',n,1,0).
+s(103841666,2,'business office',n,1,0).
+s(103953020,1,'place of business',n,1,3).
+s(103953020,2,'business establishment',n,1,0).
+s(105671676,1,'personal business',n,1,0).
+s(105833022,1,'business',n,4,12).
+s(105983801,1,'business',n,5,9).
+s(106334512,3,'doing business as',n,1,0).
+s(106425404,1,'business card',n,1,0).
+s(106485261,3,'order of business',n,1,0).
+s(106624724,1,'business letter',n,1,0).
+s(106640317,1,'business index',n,1,0).
+s(106681454,1,'business news',n,1,0).
+s(106701359,1,'master in business',n,1,0).
+s(106701359,2,'master in business administration',n,1,0).
+s(107444100,1,'business cycle',n,1,0).
+s(107575226,1,'business lunch',n,1,0).
+s(107945077,1,'business people',n,1,0).
+s(107945077,2,'businesspeople',n,1,0).
+s(107966927,1,'business',n,7,5).
+s(107966927,2,'business sector',n,1,0).
+s(107967129,1,'big business',n,1,0).
+s(108059870,3,'business firm',n,1,1).
+s(108060446,3,'manufacturing business',n,1,0).
+s(108061042,1,'business',n,1,35).
+s(108061042,3,'business concern',n,1,14).
+s(108061042,4,'business organization',n,1,1).
+s(108061042,5,'business organisation',n,1,0).
+s(108066186,1,'aluminum business',n,1,0).
+s(108066317,4,'fashion business',n,1,0).
+s(108066965,1,'computer business',n,1,0).
+s(108067951,2,'show business',n,1,0).
+s(108069050,3,'oil business',n,1,1).
+s(108069627,2,'printing business',n,1,0).
+s(108075009,2,'toy business',n,1,0).
+s(108113073,1,'businessmen',n,1,1).
+s(108113073,2,'business community',n,1,0).
+s(108118039,1,'business department',n,1,0).
+s(108281682,1,'business college',n,1,0).
+s(108281812,1,'business school',n,1,0).
+s(108352994,1,'small business administration',n,1,0).
+s(108383909,1,'family business',n,1,3).
+s(108401554,3,'business',n,8,0).
+s(108539072,1,'business district',n,1,2).
+s(108559922,1,'business address',n,1,0).
+s(109777975,1,'business agent',n,1,1).
+s(109840217,2,'big businessman',n,1,0).
+s(109840217,3,'business leader',n,1,1).
+s(109881895,1,'business editor',n,1,0).
+s(109882007,1,'businessman',n,1,10).
+s(109882615,1,'businesswoman',n,1,0).
+s(109882716,1,'businessperson',n,1,0).
+s(109883047,1,'business traveler',n,1,0).
+s(109966255,2,'business executive',n,1,1).
+s(110613052,1,'small businessman',n,1,1).
+s(113276330,1,'business expense',n,1,0).
+s(113310858,1,'business deduction',n,1,0).
+s(113345962,1,'business interruption insurance',n,1,0).
+s(113374764,1,'business loan',n,1,0).
+s(113427633,2,'business data processing',n,1,0).
+s(113836841,1,'business relation',n,1,0).
+s(113929037,2,'business relationship',n,1,1).
+s(300839959,1,'businesslike',s,1,1).
+s(301910114,1,'businesslike',s,2,0).
+s(104310347,1,'steamship company',n,1,0).
+s(105357160,1,'accompanying vein',n,1,0).
+s(106845199,1,'company name',n,1,0).
+s(108002015,1,'broadcasting company',n,1,1).
+s(108002269,1,'car company',n,1,0).
+s(108002269,2,'auto company',n,1,0).
+s(108002384,3,'dot com company',n,1,0).
+s(108002578,1,'drug company',n,1,0).
+s(108002578,2,'pharmaceutical company',n,1,0).
+s(108002717,1,'east india company',n,1,0).
+s(108003035,1,'electronics company',n,1,0).
+s(108003173,1,'film company',n,1,0).
+s(108003427,1,'food company',n,1,0).
+s(108003525,1,'furniture company',n,1,0).
+s(108003619,1,'mining company',n,1,0).
+s(108003717,1,'shipping company',n,1,0).
+s(108003839,1,'steel company',n,1,0).
+s(108003935,1,'subsidiary company',n,1,0).
+s(108004089,1,'transportation company',n,1,0).
+s(108004210,1,'trucking company',n,1,0).
+s(108058098,1,'company',n,1,60).
+s(108062464,2,'consulting company',n,1,0).
+s(108062623,4,'publishing company',n,1,0).
+s(108069241,1,'oil company',n,1,0).
+s(108069342,1,'packaging company',n,1,0).
+s(108069487,1,'pipeline company',n,1,2).
+s(108069627,3,'printing company',n,1,0).
+s(108070465,1,'insurance company',n,1,4).
+s(108070850,1,'investment company',n,1,0).
+s(108071516,2,'mutual fund company',n,1,0).
+s(108071516,4,'open-end investment company',n,1,0).
+s(108071908,2,'closed-end investment company',n,1,0).
+s(108072104,1,'face-amount certificate company',n,1,0).
+s(108076075,1,'target company',n,1,0).
+s(108077711,1,'company',n,9,0).
+s(108121394,2,'fire company',n,1,1).
+s(108184861,1,'company',n,6,3).
+s(108185211,1,'limited company',n,1,0).
+s(108185369,1,'holding company',n,1,0).
+s(108185501,1,'bank holding company',n,1,0).
+s(108185643,1,'multibank holding company',n,1,0).
+s(108185758,3,'public utility company',n,1,0).
+s(108186221,1,'telephone company',n,1,1).
+s(108186221,3,'phone company',n,1,1).
+s(108186393,1,'power company',n,1,1).
+s(108186393,3,'electric company',n,1,0).
+s(108186393,4,'light company',n,1,1).
+s(108186546,1,'water company',n,1,0).
+s(108186655,1,'gas company',n,1,0).
+s(108186761,1,'bus company',n,1,1).
+s(108186898,1,'livery company',n,1,0).
+s(108187033,1,'company',n,4,5).
+s(108187370,1,'opera company',n,1,0).
+s(108187460,1,'theater company',n,1,0).
+s(108187569,1,'stock company',n,2,0).
+s(108187569,2,'repertory company',n,1,0).
+s(108187707,1,'ballet company',n,1,1).
+s(108214272,1,'company',n,2,28).
+s(108219059,1,'ship''s company',n,1,0).
+s(108219059,2,'company',n,8,0).
+s(108234792,1,'company union',n,1,0).
+s(108264897,2,'company',n,7,0).
+s(108383310,1,'stock company',n,1,0).
+s(108383417,1,'joint-stock company',n,1,0).
+s(108420278,4,'banking company',n,1,0).
+s(108420839,1,'finance company',n,1,0).
+s(108421100,1,'consumer finance company',n,1,0).
+s(108421100,2,'small loan company',n,1,0).
+s(108421291,2,'industrial loan company',n,1,0).
+s(108421441,1,'captive finance company',n,1,0).
+s(108421644,1,'sales finance company',n,1,0).
+s(108421807,1,'commercial finance company',n,1,0).
+s(108421807,2,'commercial credit company',n,1,0).
+s(108424222,1,'trust company',n,1,0).
+s(108478261,1,'record company',n,1,0).
+s(108478482,3,'moving company',n,1,0).
+s(108478482,5,'removal company',n,1,0).
+s(109760913,2,'accompanyist',n,1,0).
+s(109887850,2,'company',n,5,3).
+s(109946437,1,'company man',n,1,0).
+s(109946574,1,'company operator',n,1,1).
+s(113929588,1,'company',n,3,6).
+s(201728355,2,'accompany',v,3,3).
+s(202025550,1,'accompany',v,2,8).
+s(202716165,2,'accompany',v,1,20).
+s(202716767,1,'company',v,1,0).
+s(202716767,3,'accompany',v,4,1).
+s(202716767,4,'keep company',v,1,3).
+s(300122844,3,'accompanying',s,1,0).
